ba579fd12566dbbb0565515d69235fe50234c7d6
Serkan Uz <serkanuz@Serkans-MacBook-Air.local>
2022-08-04 14:49:41 +0300

    lending process

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from models import *
 
 
 if __name__ == "__main__":
-    richmond_library = Library(name="Richmond Library", default_lending_day=20, default_lending_count=3)
-    twickenham_library = Library(name="Twickenham Library", default_lending_day=15, default_lending_count=2)
+    richmond_library = Library(name="Richmond Library", default_lending_day=20, default_lending_count=3, daily_penalty_for_late_give_back=2)
+    twickenham_library = Library(name="Twickenham Library", default_lending_day=15, default_lending_count=2, daily_penalty_for_late_give_back=1)
 
     book_avucunuzdaki_kelebek = Book(name="Avucunuzdaki Kelebek", isbn="68618505147")
     book_hortumlu_dunya = Book(name="Su hortumlu dunyada fil yalniz bir hayvandir", isbn="68618505148")
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     richmond_library.register_member(member_serkan_uz)
     richmond_library.register_member(member_serkan_uz)
-    
+
 
 
 
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from datetime import date, datetime, timedelta
 from decimal import Decimal
+from math import ceil
+from telnetlib import EC
 from typing import Optional
 
 
@@ -41,6 +43,7 @@ class Library:
     name: str
     default_lending_day: int 
     default_lending_count: int 
+    daily_penalty_for_late_give_back: Decimal
     members: list[Member] = field(default_factory=list)
     lending_transactions: list[LendingTransaction] = field(default_factory=list)
     payment_transactions: list[PaymentTransaction] = field(default_factory=list)
@@ -61,10 +64,29 @@ class Library:
     
     def lend_book(self, member: Member, book: Book):
 
-        pass
+        active_late_lendings = filter(lambda transaction: transaction.member == member and transaction.last_give_back_date < datetime.utcnow(), self.lending_transactions)
+        if active_late_lendings:
+            raise Exception("You are late for some books. Please, give back them first.")
+        
+        late_lendings = filter(lambda transaction: transaction.member == member and transaction.last_give_back_date < transaction.give_back_date, self.lending_transactions)
+        total_penalty = ceil(sum((transaction.give_back_date - transaction.last_give_back_date).days for transaction in late_lendings)) * self.daily_penalty_for_late_give_back
+        
+        payments = filter(lambda transaction: transaction.member == member, self.payment_transactions)
+        total_payment = sum(payment.amount for payment in payments)
 
-        # Kullanicinin borcu var mi? 
-        # Kullanici o kitabi zaten odunc almissa ve geri vermemisse
+        if total_payment < total_penalty:
+            required_payment = total_penalty - total_payment
+            raise Exception(f"Please pay your penalty. Total: {required_payment:.6f}")
+
+        active_lendings = filter(lambda transaction: transaction.member == member and transaction.is_active == True, self.lending_transactions)
+
+        if len(active_lendings) >= self.default_lending_count:
+            raise Exception("You have reached library book lending limit. Please, give back some of our books.")
+        
+        active_same_lendings = filter(lambda transaction: transaction.member == member and transaction.book == book and transaction.is_active == True, self.lending_transactions)
+        if active_same_lendings:
+            raise Exception(f"You have this book.")
+        
         # Kullanici toplamda librarynin limitine dayanmissa
 
 
